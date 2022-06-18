7b90812c416c04d7d62d2cb52c1c6617c1cbb5af
Flikendo <jalasalavidal@gmail.com>
2022-06-18 12:28:14 +0200

    Creation of the project

---
+++ src/test/java/com/flikendo/F_APIRest_Internet/FApiRestInternetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flikendo.F_APIRest_Internet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FApiRestInternetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
