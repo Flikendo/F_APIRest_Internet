25ce49b73830387d3acd51663e3d1f266970482c
Flikendo <jalasalavidal@gmail.com>
2022-06-25 23:22:59 +0200

    Updated build.gradle

--- src/main/java/com/flikendo/F_APIRest_Internet/FApiRestInternet.java
+++ src/main/java/com/flikendo/F_APIRest_Internet/FApiRestInternet.java
@@ -1,13 +1,13 @@
 package com.flikendo.F_APIRest_Internet;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class FApiRestInternet {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(FApiRestInternet.class, args);
-//	}
-//
-//}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FApiRestInternet {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FApiRestInternet.class, args);
+	}
+
+}
